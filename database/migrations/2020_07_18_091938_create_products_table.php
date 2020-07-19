a39681c0e1d14de77d5a254301f967400cee9261
Samson <samsonadejoro@gmail.com>
2020-07-19 16:33:37 +0100

    Updated README

--- database/migrations/2020_07_18_091938_create_products_table.php
+++ database/migrations/2020_07_18_091938_create_products_table.php
@@ -17,7 +17,7 @@ class CreateProductsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('description');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->float('price');
             $table->integer('quantity');
             $table->string('size');
